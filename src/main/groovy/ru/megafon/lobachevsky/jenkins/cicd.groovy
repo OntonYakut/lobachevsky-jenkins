738e951a926546352c0e7930464b5ff614729893
dmitry.travyan <dmitry.travyan@megafon.ru>
2020-09-16 14:09:05 +0300

    fix

--- src/main/groovy/ru/megafon/lobachevsky/jenkins/cicd.groovy
+++ src/main/groovy/ru/megafon/lobachevsky/jenkins/cicd.groovy
@@ -13,25 +13,23 @@ class PipelineCi implements Serializable {
     }
 
     void run() {
-        script.node() {
-            script.stage('test') {
-                script.sh(script: 'gradle test --no-build-cache --refresh-dependencies  --info', returnStdout: true)
-            }
+        script.stage('test') {
+            script.sh(script: 'gradle test --no-build-cache --refresh-dependencies  --info', returnStdout: true)
+        }
 
-            script.stage('build') {
-                script.sh(script: 'gradle build --no-build-cache --refresh-dependencies  --info', returnStdout: true)
-            }
+        script.stage('build') {
+            script.sh(script: 'gradle build --no-build-cache --refresh-dependencies  --info', returnStdout: true)
+        }
 
-            script.stage('image') {
-                script.sh(script: 'gradle image --no-build-cache --refresh-dependencies  --info', returnStdout: true)
-            }
+        script.stage('image') {
+            script.sh(script: 'gradle image --no-build-cache --refresh-dependencies  --info', returnStdout: true)
+        }
 
-            script.stage('deploy') {
-                script.docker('lobachevsky-app:v').inside() {
-                    script.sh 'java -jar lobachevsky-app-v1.0.0.jar'
-                    for (i in 0..10) {
-                        script.sh 'curl localhost:8080/hello'
-                    }
+        script.stage('deploy') {
+            script.docker('lobachevsky-app:v').inside() {
+                script.sh 'java -jar lobachevsky-app-v1.0.0.jar'
+                for (i in 0..10) {
+                    script.sh 'curl localhost:8080/hello'
                 }
             }
         }
